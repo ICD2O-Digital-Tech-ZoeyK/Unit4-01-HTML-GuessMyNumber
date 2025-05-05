2d89276afceb82dd529983c67b099d94f828e8d5
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-05 01:03:12 +0000

    random number generator

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+let number = Math.floor(Math.random() * 6) + 1;
+
+function checkGuess() {
+    let guess = document.getElementById("userGuess").value;
+    guess = parseInt(guess);
+    let result = document.getElementById("result");
+
+    if (guess === number) {
+        result.textContent = "You got it right!";
+    }
+
+    if (guess !== number) {
+        result.textContent = "Wrong guess. The correct number was " + number + ".";
+    }
+}
